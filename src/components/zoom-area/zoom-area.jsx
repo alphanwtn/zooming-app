90a881a27d255dca3087e703500affb4d9742ad4
Julien Bessac <ju.bessac@gmail.com>
2024-02-06 02:58:00 +0100

    feat: improved mobile zoom behaviour

--- src/components/zoom-area/zoom-area.jsx
+++ src/components/zoom-area/zoom-area.jsx
@@ -10,8 +10,10 @@ export default function ZoomArea({ imageSrc, imageAlt }) {
   const cursorPositionRelativeToImage = useRef(null);
   const imageRef = useRef(null);
 
+  const ZOOM_AREA_PADDING = 24;
+
   const updateCursorPosition = (e) => {
-    // disable scroll
+    // disable scroll and refresh
     if (e.type === 'touchmove') {
       e.preventDefault();
     }
@@ -24,7 +26,6 @@ export default function ZoomArea({ imageSrc, imageAlt }) {
     // mobile "touchmove" "touchstart"
     if (e.touches) {
       const { touches } = e;
-
       const posX = touches[0].clientX;
       const posY = touches[0].clientY;
 
@@ -48,28 +49,39 @@ export default function ZoomArea({ imageSrc, imageAlt }) {
     if (imageRef.current && cursorPositionAbsolute) {
       const rect = imageRef.current.getBoundingClientRect();
 
+      const isOutsideZoomArea =
+        cursorPositionAbsolute.x < rect.left - ZOOM_AREA_PADDING ||
+        cursorPositionAbsolute.x > rect.right + ZOOM_AREA_PADDING ||
+        cursorPositionAbsolute.y < rect.top - ZOOM_AREA_PADDING ||
+        cursorPositionAbsolute.y > rect.bottom + ZOOM_AREA_PADDING;
+
       const relativeImageX = (cursorPositionAbsolute.x - rect.left) / rect.width;
       const relativeImageY = (cursorPositionAbsolute.y - rect.top) / rect.height;
 
-      cursorPositionRelativeToImage.current = {
-        x: `${relativeImageX * 100}%`,
-        y: `${relativeImageY * 100}%`,
-      };
+      if (isOutsideZoomArea) {
+        cursorPositionRelativeToImage.current = null;
+      } else {
+        cursorPositionRelativeToImage.current = {
+          x: `${relativeImageX * 100}%`,
+          y: `${relativeImageY * 100}%`,
+        };
+      }
     }
   }, [cursorPositionAbsolute]);
 
   return (
-    <div className={styles.zoom_area}>
+    <div className={styles.zoom_area} style={{ padding: ZOOM_AREA_PADDING }}>
       <Image
         ref={imageRef}
         src={imageSrc}
         alt={imageAlt}
+        onDragStart={(e) => e.preventDefault()}
         layout='responsive'
         width={400}
         height={400}
         priority
       />
-      {cursorPositionAbsolute && cursorPositionRelativeToImage.current && (
+      {cursorPositionRelativeToImage.current && (
         <div
           className={styles.cursor}
           style={{
